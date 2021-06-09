2deb6b09e46c50ee5b53db87e64ffd353203c30f
Dean Cook <deanrobertcook@gmail.com>
2021-06-10 00:47:44 +0200

    fix: use dependency version of graphql client and reduce request

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 import json
 from dotenv import load_dotenv, find_dotenv
 import time
-from graphql import GraphqlClient
+from python_graphql_client import GraphqlClient
+
 
 load_dotenv(find_dotenv())
 
@@ -14,7 +15,7 @@ HEADERS = {
     'authority': 'www.glassdoor.de',
     'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
     'apollographql-client-name': 'job-search',
-    'gd-csrf-token': 'qbZ7acvPDn8NAXK6NhAyfg:1gdpXQSYnmjya-dkjlv_nhNEK8R5h_F7mQsuspkl2Ls17Ifbzx7NDfqmmDy-jWRxeLyQ4B9Jni1EvSLS-dPH1A:P8XM_8QOyRX9CIPUJ3dV5UpevV_p4FZDPqFnNpHI9EM',
+    'gd-csrf-token': '0HpLnx4PbKRisulxK22bUA:6Plt5OBEGCQmz2DHw6S8_-YYC8qNarNOfnphFAGQ-ZIzQApltc8y6GuS_pZ1Vtzr0MyBlrQpNAOlmMZQm7MTSQ:3A7k4h5lET76f0tSNWSdM7y580ZT6nRTAJZOEXQRmZk',
     'sec-ch-ua-mobile': '?0',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
     'content-type': 'application/json',
@@ -26,8 +27,9 @@ HEADERS = {
     'sec-fetch-dest': 'empty',
     'referer': 'https://www.glassdoor.de/',
     'accept-language': 'en-US;q=0.9,en;q=0.8',
-    'cookie': 'gdId=c77c7a0d-1867-4568-a4e0-cd5bf4b6887f; trs=https%3A%2F%2Fwww.google.com%2F:SEO:SEO:2021-06-09+06%3A51%3A15.487:undefined:undefined; _gid=GA1.2.1259183532.1623246679; G_ENABLED_IDPS=google; fpvc=1; OptanonAlertBoxClosed=2021-06-09T13:51:30.439Z; _optionalConsent=true; _gcl_au=1.1.1658861872.1623246691; _fbp=fb.1.1623246690902.370312596; __gads=ID=ebaab3dacb72eeeb:T=1623246691:S=ALNI_MYXvefDJxK8vgqBBKG0FsnNYffmIQ; ht=%7B%22quantcast%22%3A%5B%22D%22%5D%7D; indeedCtk=1f7ohumvj3oah001; ki_r=; ki_u=243d60fe-cc57-5525-e71e-90a3; JSESSIONID_UP_APP=4E1BA8258E1EFBE77870070E6CAE60BE; uc=44095BCBCAA84CA8D083AA33D0C199178AE8F35E9AD7F8C96075743956155E8B7639D64B8B3F63695F77B0FBAF5F06A3C4DA4682142FBDBA3FC6A7D3378C3A4E6FC7692571A8CF3499237648299ADA7B3EFA9081DD1A095CDDF0DE101785CAE097D523ED94F3B235D920FB3E6E3FB5964AD7208E146B8D97DBA9E75AAA5C08C38B2219EC613DB9E761C71275C1DC27D0; JSESSIONID_KYWI_APP=CC50061AF0B7C4B63A0D49BF1F82B8FF; ki_t=1623248074564%3B1623248074564%3B1623256439526%3B1%3B2; ki_s=198467%3A1.0.0.0.2%3B209117%3A1.0.0.1.2; asst=1623267026.0; GSESSIONID=c77c7a0d-1867-4568-a4e0-cd5bf4b6887f+1623246675455; cass=2; JSESSIONID=D719434186B4589183B0CE950D62D36B; _ga=GA1.2.1343046783.1623246679; OptanonConsent=isIABGlobal=false&datestamp=Wed+Jun+09+2021+22%3A05%3A43+GMT%2B0200+(Central+European+Summer+Time)&version=6.8.0&hosts=&consentId=2053cc3e-86db-45c8-96fd-996656693761&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1%2CC0017%3A1&geolocation=DE%3BBE&AwaitingReconsent=false; __cf_bm=67156806a2b9273cb13a180ce9ca44d73c96e755-1623274350-1800-ARtVGJTdj+n3u7Y5F7JnCrRR1VKxI4/wJ6zjCyKut2915vvbYU+WGMkxvi7iwj1tjupuGQ/ZKF3cSNUjvUjc44s=; _gat_UA-2595786-1=1; AWSALB=2PKnDS+ztHHr+HdHocQr09oyVDWhas0m2XPulL1sAnn7WwBne4n9lEnp8Zw0+LdsphvymwwCSmgr+hCoSD17ykgG3WL5GdjkcdfA9OQxMohdC1VzXPAmFq8gANNdGLfUQD/jCV2PAPORQZwTggi6BxXiqw/98VMV4EJKaE+QhAySnP+N5wcItT8swyUvtSV8j+ChInT0xoJsx9U8SGEjxzbJ+hwFUAo/Fk1D88j1PqKp8LNSDE0U80ithVkw3F4=; AWSALBCORS=2PKnDS+ztHHr+HdHocQr09oyVDWhas0m2XPulL1sAnn7WwBne4n9lEnp8Zw0+LdsphvymwwCSmgr+hCoSD17ykgG3WL5GdjkcdfA9OQxMohdC1VzXPAmFq8gANNdGLfUQD/jCV2PAPORQZwTggi6BxXiqw/98VMV4EJKaE+QhAySnP+N5wcItT8swyUvtSV8j+ChInT0xoJsx9U8SGEjxzbJ+hwFUAo/Fk1D88j1PqKp8LNSDE0U80ithVkw3F4=; ADRUM_BTa=R:0|g:e57e9ff0-2e16-494f-ade4-b9f0f262a4b0|n:glassdoor_17d346a0-2ec1-4454-86b0-73b3b787aee9; SameSite=None; JSESSIONID_JX_APP=75ABBF3AAE49501BDCC549978BF4D3DD; ADRUM_BT1=R:0|i:1188649|e:118|d:149; ADRUM_BT2=R:0|i:1188650|e:168; gdsid=1623246675455:1623274359488:B73042C81AF9F61D4372EC36926729D2; at=PUFBo_R2j_fzLMA96bdFN9V6fSoK9HB2c8iTTSEx4dSYNNb3l0fl12hBOY-UMTCYDoQu4_w0gpEWVnI-KM3Okto9zH8-v2wvzmq6quQ944VWxwJrdOK--5B_FuBU9gy0Pb2I1Vepwifri104q_HSWXzsb4gH0KxzyHs267mbcE1VH9QDRACVIyVmBLVVjCigBc3PHhNUr3OhyvRlb-g1V0kO_Igg3CL_m0fZofCacetp7v8QpQ_zHHioQL5bcc2O6NpmvvAH7m4n4KTogRm0Znl2b0e_owh42nBPVcu1dyFlXcRj8zSR3b5j6Ju-Kxp16C3ZanUhJyUck-l-FKnJlszCnBAca5P8eEyCvOqqHfSoLqpE76rhuFS9SUU8aE5bDSIH1ckN2v5U2rO4W-MT4miBuCxg6y_4pgcrfwdQ_MJ-JLe8ggD1OdS1QWvs-YtXasPVt8HZ4SiMgsKJdjUahHLmdL-I3ubdhqv0hOxnemsjyaDu4IG67DX6dzcMM65XQW7XhCXx8m8g9v7Ptu3Qx0ezThtDXlsxJ3HiT5FQ7oUCWrSsy4EpMCiPcSQiOkT55Cj6DHcFoS1ZjMTJT9Wdt4zfEFAa4joRuaM_L7GRC_aHmuKtHN5UVdyv9ocWyNmnxwoMcBnzhy2h3t61JUIcPef_lxTAOTT1g1xAT8PgeSxu-_skUClRCfnmRc4Xze-0IxJgYbXEP3jDY5emMILOXK3NOeaKThpty1PvnOoIaYt6wUXEGT_p-LMRYQFBmuCY4qbowegzCGf3FUWmutmnZpNjH4XSGrVuSNksB27rWle-gXH9iQRSv7U0wV0POHfgKNzeuSbUujKmn9yXKjz3lewQsJszDp1p-Jgyvus8cdTsQOVbnX24rmen06JhdO9f9Pk; bs=AKYIHM8Aa_wXWlXjZVT8Kg:wlzoKGl0qEkqG5Sbh5cYQDb8GJD3SL133wZo9e-c4giGfl2MafGIvyZifc8ILYif7piYKxT7iloZV_Jfhctu7B26Rq6GH9ef3eeCuntVVhY:rJ-AVkig9swPl4mtiMmw9Rfnx_kF9351VKWhXlSwbmM; _ga_RC95PMVB3H=GS1.1.1623274352.5.1.1623274364.48',
+    'cookie': 'gdId=c77c7a0d-1867-4568-a4e0-cd5bf4b6887f; trs=https%3A%2F%2Fwww.google.com%2F:SEO:SEO:2021-06-09+06%3A51%3A15.487:undefined:undefined; _gid=GA1.2.1259183532.1623246679; G_ENABLED_IDPS=google; fpvc=1; OptanonAlertBoxClosed=2021-06-09T13:51:30.439Z; _optionalConsent=true; _gcl_au=1.1.1658861872.1623246691; _fbp=fb.1.1623246690902.370312596; __gads=ID=ebaab3dacb72eeeb:T=1623246691:S=ALNI_MYXvefDJxK8vgqBBKG0FsnNYffmIQ; ht=%7B%22quantcast%22%3A%5B%22D%22%5D%7D; indeedCtk=1f7ohumvj3oah001; ki_r=; ki_u=243d60fe-cc57-5525-e71e-90a3; uc=44095BCBCAA84CA8D083AA33D0C199178AE8F35E9AD7F8C96075743956155E8B7639D64B8B3F63695F77B0FBAF5F06A3C4DA4682142FBDBA3FC6A7D3378C3A4E6FC7692571A8CF3499237648299ADA7B3EFA9081DD1A095CDDF0DE101785CAE097D523ED94F3B235D920FB3E6E3FB5964AD7208E146B8D97DBA9E75AAA5C08C38B2219EC613DB9E761C71275C1DC27D0; ki_t=1623248074564%3B1623248074564%3B1623256439526%3B1%3B2; ki_s=198467%3A1.0.0.0.2%3B209117%3A1.0.0.1.2; GSESSIONID=c77c7a0d-1867-4568-a4e0-cd5bf4b6887f+1623246675455; cass=2; asst=1623276646.0; __cf_bm=b9c827b074a990bf3a853ca53a709b67dfec200f-1623278357-1800-AXPPP0gI/zd8aMuky/kjhMPykRw4XiwrkzS33HwIbemsNRKeN+cUIRIgkhwq9uDdry3Q0qUZZjbjgaPoPDeIpJU=; _gat_UA-2595786-1=1; SameSite=None; JSESSIONID_JX_APP=90986EBC0E5779D63BE2CD4DCC1408CD; gdsid=1623246675455:1623278367436:0FF338DACD978C41E8AE905E31391EBF; at=hm14fcp-NY4kRKj2FwXdr2lpxTVu4bNCYGVdkBaF57Mqan0vbKOsKFaCnB0LP1QzWta6LfrJhDd0LVsAbmphTcaaW9GIJ7E0FCamRbhkJXw0ZoqNO2RmDYyzpnpOJW135IBb9tzItt3RbfJYW49u2_u6qTaBsZiQREyM7cuhAItjGNBHp0jpk5aBHRFBzfWe-Pjw6WULJlIHcAYf9qYWhx01NCr6Geobgzd1C9WTEwF_Y9BtjZxueMv4QY-rnLGutXwC1SvRTY2x-sSFojFgzXEtdDz5RRF6FEH6LHy9QUCUdtYcMHvQsmC-PuUYm68KSU_h5UqlsfGZYcelUT50Bke-AU26WvcCc0L4PzuvwQRbj4DYmd9oKNX42gyVFObcORp96n99gaHEIhWKGazQN1FpICb7BhEDNtmJk2YpAwWfB5vAbNjCjZRv9aVD_XkhUeUI9Fz9sSkrx9S7XjgdMSQquDb36mSvfR_o9NPOHrBJD89mx7qh-SHJyy-rtTegHUxRY0ePoOV4xy8NZY_GXRnyBoEA9udIWOoZHt0_v8jzzo6KlexYJfmd0kaW-KkU21x00xAnoyH80v8WNu12awQTlvagohfylDNSRMVwfEMv_-V_Ch7PeSZcb8a3kBK8bS_wvDOjUPmdrYFC07i114EskQ2opA29Qfy5iU-P2GpJp9k-uSdNAgvcHjYT-etkWf8eCRA9wcN9-7p9LwzhLvEX_I9HBKlON0BPiW2uS1QEScHz2UmJaanfTzxiUXUu-XENguseVyDzQ6s1WWp6JrWRxxeX56OZeR4uMx5UcGNLB9QJVDew7MhxbHmWjMcKXcIbbLeko4NzTeGn0pilp2bykwTQDZqOdmiR-dHO9bHCyXJJuq6T2XTXec811yiCwug; bs=0oz7ib6dhWDCPfzw-Vuzmw:YYaoxRRLmxmQTDtfkjY73zbI7n5trHT7XXC8QsSoviJD6l7olR3FK1_SoPtisL_3erIR5WLEm-Sg_ObRMzwFKUtkrTFVv8Ob0fpBICTUe_c:sIkwLSEstafqmn00zGBhIkDYtRYd_8iinDPVURO9TXw; JSESSIONID=305563EFA9A3CF56AAD1F4FC4690D8B2; _ga=GA1.2.1343046783.1623246679; _dc_gtm_UA-2595786-1=1; _GA_Job_Session=true; OptanonConsent=isIABGlobal=false&datestamp=Thu+Jun+10+2021+00%3A39%3A37+GMT%2B0200+(Central+European+Summer+Time)&version=6.8.0&hosts=&consentId=2053cc3e-86db-45c8-96fd-996656693761&interactionCount=2&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1%2CC0017%3A1&geolocation=DE%3BBE&AwaitingReconsent=false; AWSALB=qAQu7H0JJYLMoOzz07XdxDufajgYtVd4AVlOvw4lVNbMiSmMwCpPTTTtFzpTWzyOCSMjQYJ2xpL6V0hMztK3aQnrpN1mDKdokIsYOPgM1eGX+jJCCsFwKeVQ0NJ/ew90M32cNeNgW4PxkXDpsOBqc1eboe/Qq85EIeWAY3VDB17m6HuL2I2IvQPCT426ep98SWKZkwEa2+WbJm5AwUbrov7s0i3JT+v+SOydEQLQODzjL4F8ghzar8VQiZtemi8=; AWSALBCORS=qAQu7H0JJYLMoOzz07XdxDufajgYtVd4AVlOvw4lVNbMiSmMwCpPTTTtFzpTWzyOCSMjQYJ2xpL6V0hMztK3aQnrpN1mDKdokIsYOPgM1eGX+jJCCsFwKeVQ0NJ/ew90M32cNeNgW4PxkXDpsOBqc1eboe/Qq85EIeWAY3VDB17m6HuL2I2IvQPCT426ep98SWKZkwEa2+WbJm5AwUbrov7s0i3JT+v+SOydEQLQODzjL4F8ghzar8VQiZtemi8=; _ga_RC95PMVB3H=GS1.1.1623278358.6.1.1623278388.30',
 }
+
 
 def main():
     job_title = 'data science'#input('Job search input: ')
@@ -49,7 +51,7 @@ def tidyJson():
 
 def graphQLTest():
     client = GraphqlClient(endpoint='https://www.glassdoor.de/graph', headers=HEADERS)
-    variables = {'enableReviewSummary':False, 'jl':4074701403, 'queryString': 'pos=101&ao=1044074&s=149&guid=00000179f2b11edb8d68c1103c7985a9&src=GD_JOB_AD&t=SRFJ&vt=w&uido=E72EB9FF023BC985E69A0D4700D2593A&cs=1_73c42787&cb=1623274365290&jobListingId=4074701403' }
+    variables = {'enableReviewSummary':False, 'jl':4105061587, 'queryString': 'pos=102&ao=1044074&s=149&guid=00000179f2ee36b89cc87c68dd88fcdd&src=GD_JOB_AD&t=SRFJ&vt=w&uido=E72EB9FF023BC985E69A0D4700D2593A&cs=1_09fd0108&cb=1623278368915&jobListingId=4105061587' }
     query = """
         query JobDetailQuery($jl: Long!, $queryString: String, $enableReviewSummary: Boolean!) {
           jobView(listingId: $jl, contextHolder: { queryString: $queryString }) {
@@ -71,77 +73,6 @@ def graphQLTest():
         }
 
         fragment DetailFragment on JobView {
-          employerBenefits {
-            benefitsOverview {
-              benefitsHighlights {
-                benefit {
-                  commentCount
-                  icon
-                  name
-                  __typename
-                }
-                highlightPhrase
-                __typename
-              }
-              overallBenefitRating
-              employerBenefitSummary {
-                comment
-                __typename
-              }
-              __typename
-            }
-            benefitReviews {
-              benefitComments {
-                id
-                comment
-                __typename
-              }
-              cityName
-              createDate
-              currentJob
-              rating
-              stateName
-              userEnteredJobTitle
-              __typename
-            }
-            numReviews
-            __typename
-          }
-          employerContent {
-            featuredVideoLink
-            managedContent {
-              id
-              type
-              title
-              body
-              captions
-              photos
-              videos
-              __typename
-            }
-            diversityContent {
-              goals {
-                id
-                workPopulation
-                underRepresentedGroup
-                currentMetrics
-                currentMetricsDate
-                representationGoalMetrics
-                representationGoalMetricsDate
-                __typename
-              }
-              __typename
-            }
-            __typename
-          }
-          employerAttributes {
-            attributes {
-              attributeName
-              attributeValue
-              __typename
-            }
-            __typename
-          }
           header {
             jobLink
             adOrderId
@@ -205,163 +136,6 @@ def graphQLTest():
               labelKey
               messageKey
               normalizedCount
-              __typename
-            }
-            __typename
-          }
-          similarJobs {
-            relatedJobTitle
-            careerUrl
-            __typename
-          }
-          job {
-            description
-            discoverDate
-            eolHashCode
-            importConfigId
-            jobReqId
-            jobSource
-            jobTitleId
-            jobTitleText
-            listingId
-            __typename
-          }
-          map {
-            address
-            country
-            employer {
-              id
-              name
-              __typename
-            }
-            locationName
-            postalCode
-            __typename
-          }
-          overview {
-            ceo {
-              name
-              photoUrl
-              __typename
-            }
-            id
-            name
-            shortName
-            squareLogoUrl
-            headquarters
-            links {
-              overviewUrl
-              benefitsUrl
-              photosUrl
-              reviewsUrl
-              salariesUrl
-              __typename
-            }
-            primaryIndustry {
-              industryId
-              industryName
-              sectorName
-              sectorId
-              __typename
-            }
-            ratings {
-              compensationAndBenefitsRating
-              cultureAndValuesRating
-              careerOpportunitiesRating
-              workLifeBalanceRating
-              __typename
-            }
-            overview {
-              description
-              __typename
-            }
-            revenue
-            size
-            type
-            website
-            yearFounded
-            __typename
-          }
-          photos {
-            photos {
-              caption
-              photoId
-              photoId2x
-              photoLink
-              photoUrl
-              photoUrl2x
-              __typename
-            }
-            __typename
-          }
-          rating {
-            ceoApproval
-            ceoRatingsCount
-            employer {
-              name
-              __typename
-            }
-            recommendToFriend
-            starRating
-            __typename
-          }
-          reviews {
-            reviews {
-              advice
-              cons
-              countHelpful
-              employerResponses {
-                response
-                responseDateTime
-                userJobTitle
-                __typename
-              }
-              employmentStatus
-              featured
-              isCurrentJob
-              jobTitle {
-                text
-                __typename
-              }
-              lengthOfEmployment
-              pros
-              ratingBusinessOutlook
-              ratingCareerOpportunities
-              ratingCeo
-              ratingCompensationAndBenefits
-              ratingCultureAndValues
-              ratingOverall
-              ratingRecommendToFriend
-              ratingSeniorLeadership
-              ratingWorkLifeBalance
-              reviewDateTime
-              reviewId
-              summary
-              __typename
-            }
-            __typename
-          }
-          salary {
-            currency {
-              code
-              numOfDecimals
-              negativeFormat
-              positiveFormat
-              symbol
-              __typename
-            }
-            lastSalaryDate
-            salaries {
-              count
-              maxBasePay
-              medianBasePay
-              minBasePay
-              jobTitle {
-                id
-                text
-                __typename
-              }
-              payPeriod
               __typename
             }
             __typename

cd2ef8fc4dee9cd090dc00e7555c5daee56ec7ae
Dean <deanrobertcook@gmail.com>
2021-06-09 19:28:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Job Scraper
+
+A web scraper designed to fetch job postings from various websites and store them somewhere.
---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 09:27:21 2020
+
+@author: vlad
+"""
+import os
+import requests
+from dotenv import load_dotenv, find_dotenv
+from scraper import scraper
+
+load_dotenv(find_dotenv())
+
+GD_USERNAME = os.environ.get("GD_USERNAME")
+GD_PASSWORD = os.environ.get("GD_PASSWORD")
+
+def main():
+    print('Glassdoor Job Scraper.')
+    job_title = 'data science'#input('Job search input: ')
+    location = ''#input('Location: ')
+
+    s = requests.Session()
+    #s.headers.update({'x-test': 'true'})
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    url = 'https://www.glassdoor.de/graph'
+    res = requests.post(url, headers=headers, data = {'key':'value'})
+    print(res)
+
+
+
+def getPage():
+    res = requests.get('https://www.glassdoor.de/Job/berlin-data-science-jobs-SRCH_IL.0,6_IC2622109_KO7,19.htm', headers=headers)
+
+    outF = open("output/myOutFile.html", "wb")
+    outF.write(res.content)
+    outF.close()
+
+if __name__ == "__main__":
+    main()
